4c82042b33df8507062009a10d08fc776b48088d
Borkata <borispenev89@gmail.com>
2013-08-14 19:40:31 -0700

    Added BetController and BetModel

---
+++ BetMania/BetMania.Services/Controllers/BetController.cs
@@ -0,0 +1,71 @@
+﻿using BetMania.Models;
+using BetMania.Repositories;
+using BetMania.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BetMania.Services.Controllers
+{
+    public class BetController : ApiController
+    {
+        private IRepository<Bet> betRepository;
+
+        public BetController(IRepository<Bet> betRep)
+        {
+            this.betRepository = betRep;
+        }
+
+        // GET api/bet
+        public IEnumerable<BetModel> Get()
+        {
+            var bets = this.betRepository.All();
+
+            var betModel = from betEntity in bets
+                            select new BetModel
+                            {
+                                 Id = betEntity.Id,
+                                 MakeBet = betEntity.MakeBet,
+                                 BetType = betEntity.BetType,
+                                 Match = betEntity.Match,
+                                 User = betEntity.User
+                            };
+            return betModel;
+        }
+
+        // GET api/bet/5
+        public Bet Get(int id)
+        {
+            return this.betRepository.Get(id);
+        }
+
+        // POST api/bet
+        public Bet Post(Bet value)
+        {
+            return this.betRepository.Add(value);
+        }
+
+        // PUT api/bet/5
+        public void Put(int id, Bet value)
+        {
+            if (ModelState.IsValid)
+            {
+                if (value.Id != id)
+                {
+                    throw new ArgumentException("Not user with such id");
+                }
+
+                this.betRepository.Update(value);
+            }
+        }
+
+        // DELETE api/bet/5
+        public void Delete(int id)
+        {   
+            this.betRepository.Delete(id);
+        }
+    }
+}
---
+++ BetMania/BetMania.Services/Models/BetModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetMania.Services.Models
+{
+    public class BetModel
+    {
+        public int Id { get; set; }
+        public decimal MakeBet { get; set; }
+
+        // Navigation Properties
+        public virtual User User { get; set; }
+        public virtual Match Match { get; set; }
+        public virtual BetType BetType { get; set; }
+    }
+}
