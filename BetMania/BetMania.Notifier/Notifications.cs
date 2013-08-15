7712820b148f4f0b50ea2a8fc54ce008b10ed68f
Borkata <borispenev89@gmail.com>
2013-08-15 16:21:47 -0700

    Added Notifications and a console project for testing

---
+++ BetMania/BetMania.Notifier/Notifications.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BetMania.Notifier
+{
+    public static class Notifications
+    {
+        public const string PUBLISH_KEY = "pub-c-f4fde725-6da4-4350-b8f1-ec891116f55a";
+        public const string SUBSCRIBE_KEY = "sub-c-8743658c-04be-11e3-a5e8-02ee2ddab7fe";
+        public const string SECRET_KEY = "sec-c-NjMxZTYxMTAtZGM5NC00NWQ2LTliMzUtODNlZmY4M2Q4Zjhj";
+        public const bool SSL_ON = true;
+        public static Pubnub pubnub = new Pubnub(PUBLISH_KEY, SUBSCRIBE_KEY, SECRET_KEY, SSL_ON);
+        public const string Channel = "The_Joker";
+              
+        public static void Publish(string message)
+        {
+            pubnub.publish(Channel, message);
+        }
+    }
+   
+}
---
+++ BetMania/NotificationsTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using BetMania.AvatarUploader;
+using BetMania.Notifier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NotificationsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Notifications.Publish("Test");
+            AvatarUploader avatarUploder = new AvatarUploader();
+
+            string pathOfTheAvatar = AvatarUploader.Upload("baiGanio", @"C:\google.jpeg");
+            Notifications.Publish(pathOfTheAvatar);
+        }
+    }
+}
